49eaa762a147422f88222348210c029103504d7e
freeohariki1998 <freeohariki1998@gmail.com>
2026-01-07 01:03:50 +0900

    chore: setup backend gradle wrapper and project structure

---
+++ backend/src/test/java/com/kakeibo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakeibo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
