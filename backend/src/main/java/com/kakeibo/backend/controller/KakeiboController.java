72f8d21eea4084ffb5de94f7abf0d6c01dc3297e
freeohariki1998 <freeohariki1998@gmail.com>
2026-01-13 21:26:28 +0900

    refactor: フィールド注入をコンストラクタ注入に変更

--- backend/src/main/java/com/kakeibo/backend/controller/KakeiboController.java
+++ backend/src/main/java/com/kakeibo/backend/controller/KakeiboController.java
@@ -4,21 +4,24 @@ package com.kakeibo.backend.controller;
 import com.kakeibo.backend.entity.Kakeibo;
 import com.kakeibo.backend.repository.CategorySummary;
 import com.kakeibo.backend.repository.KakeiboRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController // 結果はJOSNで返す宣言
 @RequestMapping("/api/kakeibo")
 @CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor // コンストラクタを自動生成(finalだけ)
 public class KakeiboController {
     private final KakeiboRepository repository;
-
-    public KakeiboController(KakeiboRepository repository){
-        this.repository = repository;
-    }
 
     @GetMapping
     public List<Kakeibo> getAllKakeibo(){
@@ -35,8 +38,13 @@ public class KakeiboController {
     }
 
     @PostMapping
-    public Kakeibo create(@RequestBody Kakeibo kakeibo) {
-        return repository.save(kakeibo);
+    public ResponseEntity<?> create(@Valid @RequestBody Kakeibo kakeibo, BindingResult result) {
+        if (result.hasErrors()) {
+            // 最初のエラーメッセージを取得してエラーコード 400 で返す
+            String message = result.getAllErrors().getFirst().getDefaultMessage();
+            return ResponseEntity.badRequest().body(Map.of("message", Objects.requireNonNull(message)));
+        }
+        return ResponseEntity.ok(repository.save(kakeibo));
     }
 
     @DeleteMapping("/{id}")
--- backend/src/main/java/com/kakeibo/backend/controller/MasterDataController.java
+++ backend/src/main/java/com/kakeibo/backend/controller/MasterDataController.java
@@ -3,6 +3,7 @@ package com.kakeibo.backend.controller;
 import com.kakeibo.backend.entity.Kakeibo;
 import com.kakeibo.backend.entity.MasterData;
 import com.kakeibo.backend.repository.MasterDataRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,10 +12,10 @@ import java.util.List;
 @RestController // 結果はJOSNで返す宣言
 @RequestMapping("/api/master")
 @CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor // コンストラクタを自動生成(finalだけ)
 public class MasterDataController {
 
-    @Autowired
-    private MasterDataRepository repository;
+    private final MasterDataRepository repository;
 
     @GetMapping("/{type}")
     public List<MasterData> getByMasterType(@PathVariable String type){
